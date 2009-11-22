17251dc180420a843b0b7c949aadda900b289df0
Michael Schout <mschout@cpan.org>
2009-11-21 23:28:07 -0600

    update so mod_perl2 >= 2.0 RC5 is supported

--- lib/Apache/Singleton/Request.pm
+++ lib/Apache/Singleton/Request.pm
@@ -8,27 +8,26 @@ use Apache::Singleton;
 use base qw(Apache::Singleton);
 
 BEGIN { 
-	use constant MP2 => eval { require mod_perl; $mod_perl::VERSION > 1.99 };
-	die "mod_perl is required to run this module: $@" if $@;
+    use constant MP2 => $mod_perl::VERSION >= 1.99 ? 1 : 0;
 
-	if (MP2) { 
-		require Apache::RequestUtil; 
-	}
-	else { 
-		require Apache;
-	}
+    if (MP2) {
+        require Apache2::RequestUtil;
+    }
+    else {
+        require Apache;
+    }
 }
 
 sub _get_instance {
     my $class = shift;
-    my $r = Apache->request;
+    my $r = MP2 ? Apache2::RequestUtil->request : Apache->request;
     my $key = "apache_singleton_$class";
     return $r->pnotes($key);
 }
 
 sub _set_instance {
     my($class, $instance) = @_;
-    my $r = Apache->request;
+    my $r = MP2 ? Apache2::RequestUtil->request : Apache->request;
     my $key = "apache_singleton_$class";
     $r->pnotes($key => $instance);
 }
